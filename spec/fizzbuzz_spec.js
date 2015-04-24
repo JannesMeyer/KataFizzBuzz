2b5e29b03f5bdc56747e8173e68e0df5798be72c
Jannes <jannes.meyer@gmail.com>
2015-04-24 23:07:14 +0200

    Change tabs to two spaces

--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -3,47 +3,47 @@ var divisibleBy = require('..').divisibleBy;
 
 describe("divisibleBy()", function() {
 
-	it("returns true when the second argument is divisible by the first argument", function() {
-		var divisibleByOne   = divisibleBy(1);
-		var divisibleByTwo   = divisibleBy(2);
-		expect(divisibleByTwo(6)).toBe(true);
-		expect(divisibleByOne(1)).toBe(true);
-	});
+  it("returns true when the second argument is divisible by the first argument", function() {
+    var divisibleByOne   = divisibleBy(1);
+    var divisibleByTwo   = divisibleBy(2);
+    expect(divisibleByTwo(6)).toBe(true);
+    expect(divisibleByOne(1)).toBe(true);
+  });
 
-	it("returns false when the second argument is not divisible by the first argument", function() {
-		var divisibleByTwo   = divisibleBy(2);
-		var divisibleByThree = divisibleBy(3);
-		var divisibleByTen   = divisibleBy(10);
-		expect(divisibleByTwo(3)).toBe(false);
-		expect(divisibleByThree(2)).toBe(false);
-		expect(divisibleByTen(25)).toBe(false);
-	});
+  it("returns false when the second argument is not divisible by the first argument", function() {
+    var divisibleByTwo   = divisibleBy(2);
+    var divisibleByThree = divisibleBy(3);
+    var divisibleByTen   = divisibleBy(10);
+    expect(divisibleByTwo(3)).toBe(false);
+    expect(divisibleByThree(2)).toBe(false);
+    expect(divisibleByTen(25)).toBe(false);
+  });
 
 });
 
 describe("fizzbuzz()", function() {
 
-	it("returns the number as a String when it isn't divisible by 3 or 5", function() {
-		expect(fizzbuzz(7)).toEqual('7');
-		expect(fizzbuzz(79)).toEqual('79');
-	});
+  it("returns the number as a String when it isn't divisible by 3 or 5", function() {
+    expect(fizzbuzz(7)).toEqual('7');
+    expect(fizzbuzz(79)).toEqual('79');
+  });
 
-	it("returns 'Fizz' for numbers divisible by 3", function() {
-		expect(fizzbuzz(3)).toEqual('Fizz');
-		expect(fizzbuzz(99)).toEqual('Fizz');
-	});
+  it("returns 'Fizz' for numbers divisible by 3", function() {
+    expect(fizzbuzz(3)).toEqual('Fizz');
+    expect(fizzbuzz(99)).toEqual('Fizz');
+  });
 
-	it("returns 'Buzz' for numbers divisible by 5", function() {
-		expect(fizzbuzz(10)).toEqual('Buzz');
-		expect(fizzbuzz(50)).toEqual('Buzz');
-	});
+  it("returns 'Buzz' for numbers divisible by 5", function() {
+    expect(fizzbuzz(10)).toEqual('Buzz');
+    expect(fizzbuzz(50)).toEqual('Buzz');
+  });
 
-	it("returns 'FizzBuzz' for numbers divisible by 3 and by 5", function() {
-		expect(fizzbuzz(15)).toEqual('FizzBuzz');
-		expect(fizzbuzz(90)).toEqual('FizzBuzz');
-	});
+  it("returns 'FizzBuzz' for numbers divisible by 3 and by 5", function() {
+    expect(fizzbuzz(15)).toEqual('FizzBuzz');
+    expect(fizzbuzz(90)).toEqual('FizzBuzz');
+  });
 
-	it("fails for numbers less than one", function() {
-		expect(fizzbuzz.bind(null, 0)).toThrow();
-	});
+  it("fails for numbers less than one", function() {
+    expect(fizzbuzz.bind(null, 0)).toThrow();
+  });
 });
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,33 +2,33 @@ exports.fizzbuzz = fizzbuzz;
 exports.divisibleBy = divisibleBy;
 
 function fizzbuzz(n) {
-	if (n < 1) {
-		throw new Error("Not defined for n < 1");
-	}
-	if (divisibleBy3(n) && divisibleBy5(n)) {
-		return 'FizzBuzz';
-	}
-	if (divisibleBy3(n)) {
-		return 'Fizz';
-	}
-	if (divisibleBy5(n)) {
-		return 'Buzz';
-	}
-	return String(n);
+  if (n < 1) {
+    throw new Error("Not defined for n < 1");
+  }
+  if (divisibleBy3(n) && divisibleBy5(n)) {
+    return 'FizzBuzz';
+  }
+  if (divisibleBy3(n)) {
+    return 'Fizz';
+  }
+  if (divisibleBy5(n)) {
+    return 'Buzz';
+  }
+  return String(n);
 }
 
 var divisibleBy3 = divisibleBy(3);
 var divisibleBy5 = divisibleBy(5);
 
 function divisibleBy(divisor) {
-	return function(n) {
-		return n % divisor === 0;
-	}
+  return function(n) {
+    return n % divisor === 0;
+  }
 }
 
 // Only execute this when running directly on the console
 if (require.main === module) {
-	for (var n = 1; n <= 100; ++n) {
-		console.log(fizzbuzz(n));
-	}
+  for (var n = 1; n <= 100; ++n) {
+    console.log(fizzbuzz(n));
+  }
 }
